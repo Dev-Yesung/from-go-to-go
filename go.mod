471715a4d25e8e7a742510f7501e9b30f964c2d4
Dev-Yesung <yeasung67@gmail.com>
2025-03-26 01:09:12 +0900

    helloWorld.go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module from-go-to-go
+
+go 1.24
---
+++ helloWorld.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	s := "Gopher"
+	fmt.Printf("Hello and welcome, %s!\n", s)
+
+	for i := 1; i <= 5; i++ {
+		fmt.Printf("i = %d\n", 100/i)
+	}
+}
